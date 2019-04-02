25b548d0529dddacdbd412d683fb92d52a58c9f2
itcuihao <it_cuihao@163.com>
2019-04-02 08:29:42 +0800

    30

--- sword_offer/chapter5/5.2/topic30/run.go
+++ sword_offer/chapter5/5.2/topic30/run.go
@@ -1,7 +1,19 @@
 package topic30
 
-// O(n)
-func Run(numbers []int) int {
+// 时间复杂度为O(kn)。缺点就是，时间复杂度大了点，关键是改变了原有的输入数组。
+func Run1(numbers []int, k int) []int {
+	list := make([]int, 0, k)
+	if numbers == nil || len(numbers) < k {
+		return list
+	}
 
-	return 0
+	for i := 0; i < k; i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i] > numbers[j] {
+				numbers[i], numbers[j] = numbers[j], numbers[i]
+			}
+		}
+	}
+	list = numbers[:k]
+	return list
 }
--- sword_offer/chapter5/5.2/topic30/run_test.go
+++ sword_offer/chapter5/5.2/topic30/run_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestRun(t *testing.T) {
 	// numbers := []int{1, 2, 3, 2, 2, 2, 5, 4, 2}
 	numbers := []int{3, 2, 1, 1}
-	i := Run(numbers)
+	i := Run1(numbers, 2)
 	t.Log(numbers)
 	t.Log(i)
 }
