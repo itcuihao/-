f852db03632db7368d625df21f21d63d29575bac
itcuihao <it_cuihao@163.com>
2020-11-29 23:45:08 +0800

    update

--- gist/run.go
+++ gist/run.go
@@ -20,3 +20,25 @@ func hump2Underscore(name string) string {
 	}
 	return builder.String()
 }
+
+func binarySearch(target int, haystack []int) bool {
+
+	low := 0
+	high := len(haystack) - 1
+
+	for low <= high {
+		median := (low + high) / 2
+
+		if haystack[median] < target {
+			low = median + 1
+		} else {
+			high = median - 1
+		}
+	}
+
+	if low == len(haystack) || haystack[low] != target {
+		return false
+	}
+
+	return true
+}
--- gist/run_test.go
+++ gist/run_test.go
@@ -6,3 +6,9 @@ func TestHum2Underscore(t *testing.T) {
 	h := hump2Underscore("baseTile")
 	t.Log(h)
 }
+
+func TestBinarySearch(t *testing.T) {
+	a := []int{31}
+	index := binarySearch(31, a)
+	t.Log(index)
+}
